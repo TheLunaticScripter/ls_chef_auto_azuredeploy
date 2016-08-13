d13044268db1f41968b6a49ba9179c92db6291fb
John Snow <thelunaticscripter@outlook.com>
2016-08-13 12:43:03 -0400

    Added server deployment script

---
+++ ls_serverdeploy.ps1
@@ -0,0 +1,12 @@
+﻿# -------------
+# Script to deploy Chef Automate and dependent services to Microsoft Azure
+#
+#
+# Written by: John Snow, The Lunatic Scripter
+#
+#
+# Licensed under the Apache 2.0 Creative commons License
+#
+#
+# -------------
+
