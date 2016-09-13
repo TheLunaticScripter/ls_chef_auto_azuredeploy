a6b1f74844516b6c94ca6e8ddd23204555b3da0e
John Snow <thelunaticscripter@outlook.com>
2016-09-12 20:32:08 -0400

    Updated server deployments with CentOS 6.8 and fixed the bug that prevented creating new resource group, storage, and network

--- ls_chefsrv_deploy.ps1
+++ ls_chefsrv_deploy.ps1
@@ -23,6 +23,7 @@ $SubscriptionName = 'Visual Studio Ultimate with MSDN'
 $Location = 'East US' ### Use "Get-AzureLocation | Where-Object Name -eq 'ResourceGroup' | Format-Table Name, LocationsString -Wrap" in ARM mode to find locations which support Resource Groups
 $GroupName = 'chef-lab'
 $DeploymentName = 'chef-server-deployment'
+$StorageName = 'chefstorage' 
 $PublicDNSName = 'chef-lab-' + (New-RandomString)
 $AdminUsername = 'chef'
 
@@ -42,7 +43,7 @@ Login-AzureRmAccount
 
 ### Create Resource Group ###
 
-if((Get-AzureRmResourceGroup -ResourceGroupName $GroupName) -eq $false){
+if((Get-AzureRmResourceGroup -ResourceGroupName $GroupName).Count -eq 0){
     New-AzureRmResourceGroup -Name $GroupName -Location $Location -Verbose
     $ResourceGroup = Get-AzureRmResourceGroup -Name $GroupName
     }
@@ -53,18 +54,19 @@ $AzureResourceGroup = Get-AzureRmResourceGroup -Name $GroupName
 Write-Host 'Resource Group Name is'$AzureResourceGroup.ResourceGroupName
 
 ### Get Storage Account ###
-$AzureStorageAccount = ($AzureResourceGroup | Get-AzureRmStorageAccount).StorageAccountName
+
+if(($AzureResourceGroup | Get-AzureRmStorageAccount).StorageAccountName -like "*chefstorage*"){
+    $AzureStorageAccount = ($AzureRessourceGroup | Get-AzureRmStorageAccount).StorageAccountName
+}
+else{
+    $AzureStorageAccount = $StorageName + (New-RandomString)
+}
 Write-Host 'Storage Account is' $AzureStorageAccount
 
-### Get Virtual Network ###
-$AzureVirtualNetwork = ($AzureResourceGroup | Get-AzureRmVirtualNetwork).Name
-Write-Host 'Virtual Network is' $AzureVirtualNetwork
-
 $parameters = @{
-    'StorageAccountName'="$AzureStorageAccount";
+    'newStorageAccountName'="$AzureStorageAccount";
     'adminUsername'="$AdminUsername";
     'dnsNameForPublicIP'="$PublicDNSName";
-    'virtualNetworkName'="$AzureVirtualNetwork"
     }
 
 # Deploy Chef Server
@@ -77,6 +79,10 @@ New-AzureRmResourceGroupDeployment `
 
 # Deploy Chef Automate Server
 ### Define variables
+
+### Get Virtual Network ###
+$AzureVirtualNetwork = ($AzureResourceGroup | Get-AzureRmVirtualNetwork).Name
+Write-Host 'Virtual Network is' $AzureVirtualNetwork
 
 $DeploymentName = 'chef-auto-deployment'
 $PublicDNSName = 'chef-lab-' + (New-RandomString)
@@ -177,5 +183,25 @@ New-AzureRmResourceGroupDeployment `
     -Name $DeploymentName `
     -ResourceGroupName $AzureResourceGroup.ResourceGroupName `
     -TemplateFile azure_chefbld2.json `
+    -TemplateParameterObject $parameters `
+    -Verbose
+    
+# Deploy Chef Supermarket
+### Define variables
+$DeploymentName = 'chef-auto-deployment'
+$PublicDNSName = 'chef-lab-' + (New-RandomString)
+$AdminUsername = 'chef'
+
+$parameters = @{
+    'StorageAccountName'="$AzureStorageAccount";
+    'adminUsername'="$AdminUsername";
+    'dnsNameForPublicIP'="$PublicDNSName";
+    'virtualNetworkName'="$AzureVirtualNetwork"
+    }
+
+New-AzureRmResourceGroupDeployment `
+    -Name $DeploymentName `
+    -ResourceGroupName $AzureResourceGroup.ResourceGroupName `
+    -TemplateFile azure_chefmkt.json `
     -TemplateParameterObject $parameters `
     -Verbose
